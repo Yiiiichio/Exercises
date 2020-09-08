f0eba9136dfdd48c8231351e2c267fbd8c600579
yiqiuKLFMedia <yiqiu@klfmedia.com>
2020-09-08 17:05:07 -0400

    The code about how to use AtomicInteger

--- AtomicIntegerExercise/src/Main.java
+++ AtomicIntegerExercise/src/Main.java
@@ -1,2 +1,68 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        //it should print 0
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class DecrementingThread extends Thread {
+
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private AtomicInteger items = new AtomicInteger(0);
+
+        public void increment() {
+            items.incrementAndGet();//increment the items by one, return the new value
+            //items.getAndIncrement();//increment the items by one, return the previous value
+        }
+
+        public void decrement() {
+            items.decrementAndGet();//decrement the items by one, return the new value
+            //items.getAndDecrement();//decrement the items by one, return the previous value
+        }
+
+        public int getItems() {
+            return items.get();
+        }
+    }
 }
